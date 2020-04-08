6d587c1308a9de2a9aebaa3537e56c6d527855ea
Your NameDonyJ <867256709@qq.com>
2020-04-08 23:59:04 +0800

    #招聘会信息展示调整

--- xmtx-webui/src/main/java/com/xmtx/webui/controller/jobfairController.java
+++ xmtx-webui/src/main/java/com/xmtx/webui/controller/jobfairController.java
@@ -2,40 +2,40 @@ package com.xmtx.webui.controller;
 
 import com.xmtx.common.VO.ResultVO;
 import com.xmtx.jobfair.client.JobFairClient;
-import com.xmtx.jobfair.common.ReleaseInput;
-import com.xmtx.jobfair.form.JobFairForm_Release;
+import com.xmtx.jobfair.common.JobFairForm_Release;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.Map;
 
 
-@Controller
+@RestController
+@RequestMapping("/jobfair")
 @Slf4j
+@Service
 public class jobfairController {
     @Autowired
     private JobFairClient jobFairClient;
-//    @RequestMapping("/jobfair/getCommandList")
-//    @ResponseBody
-//    public ResultVO recommendJobsList(@RequestParam("pn") Integer pn){
-////        jobFairClient.list()
-//        ResultVO result= jobFairClient.list(pn);
-//        log.error("测试");
-//        return result;
-//    }
+    @RequestMapping("/getCommandList")
+    @ResponseBody
+    public ResultVO recommendJobsList(@RequestParam("pn") Integer pn){
+//        jobFairClient.list()
+        ResultVO result= jobFairClient.list(pn);
+        log.error("测试");
+        return result;
+    }
 
-    @PostMapping(value = "jobfair/release")
+    @PostMapping("/release")
+    @ResponseBody
     public String release(
-            @Valid ReleaseInput releaseInput,
+            @Valid JobFairForm_Release releaseInput,
             BindingResult bindingResult){
         jobFairClient.release(releaseInput,bindingResult);
         System.out.println(bindingResult.toString());
