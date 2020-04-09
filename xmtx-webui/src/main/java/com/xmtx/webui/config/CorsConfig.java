9853c7877decee2577ec8662447c738fdb327f96
Your NameDonyJ <867256709@qq.com>
2020-04-09 14:14:56 +0800

    #招聘会信息展示调整

---
+++ xmtx-webui/src/main/java/com/xmtx/webui/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.xmtx.webui.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+/**
+ * @ Author     ：djq.
+ * @ Date       ：Created in 12:54 PM 4/9/2020
+ * @ Description：
+ * @ Modified By：
+ * @Version: $
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setMaxAge(300L);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
